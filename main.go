87f99926eb5aa733146a8611a6db89ffe84efe49
Marcos Jauregui <it.marcosj@gmail.com>
2021-12-31 15:41:24 -0600

    welcome message

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "log"
+
+func main() {
+	log.Println("▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█")
+	log.Println("  Welcome to the WZ Bootcamp")
+	log.Printf("▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n\n\n")
+}
